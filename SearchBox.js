17038f9b9f3551858f33efa8b39b160f5fada5bc
Bocanet Cristi <70154549+boka4u@users.noreply.github.com>
2021-01-15 16:45:39 +0200

    Add files via upload

---
+++ SearchBox.js
@@ -0,0 +1,61 @@
+import React, { useState } from 'react'
+import { Button, TextField } from '@material-ui/core'
+import AddIcon from '@material-ui/icons/Add'
+import { searchMovies } from '../shared/API'
+
+import styles from './SearchBox.module.css'
+
+const MovieList = (props) => {
+  return (
+    <div>
+      <ul>
+        {props.movies.map((movie) => (
+          <li className={styles.listItem} key={movie.id}>
+            <b>{movie.title}</b> ({movie.release_date})
+            <Button
+              className="add_movie"
+              color="secondary"
+              onClick={(e) => {
+                e.preventDefault()
+                props.onMovieAdd(movie)
+              }}>
+              <AddIcon /> Add movie
+            </Button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
+}
+
+const SearchBox = (props) => {
+  const [term, setTerm] = useState('')
+  const [movies, setMovies] = useState([])
+
+  const localMovieAdd = (movie) => {
+    setMovies([])
+    props.onMovieAdd(movie)
+  }
+  return (
+    <div className={styles.main}>
+      <TextField
+        label="Search for a movie"
+        variant="outlined"
+        value={term}
+        onChange={(e) => {
+          setTerm(e.target.value)
+        }}
+      />
+      <Button
+        variant="contained"
+        color="primary"
+        onClick={() =>
+          searchMovies(term).then((res) => setMovies(res.data.results))
+        }>
+        Search
+      </Button>
+      <MovieList movies={movies} onMovieAdd={localMovieAdd} />
+    </div>
+  )
+}
+export default SearchBox
